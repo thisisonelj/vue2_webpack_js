a054d9aaf2569d474928d3b5eab27cdf784e12b9
“lj” <liujun01@jiuqi.com.cn>
2023-02-09 13:11:02 +0800

    提交1

--- demo-vue-create/router/index.js
+++ demo-vue-create/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import displayPage from '@/components/display-page';
 Vue.use(Router);
 export default new Router({
   routes: [
@@ -8,6 +9,11 @@ export default new Router({
       path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'displayPage',
+      component: displayPage
     }
   ]
 });
