3ee6c4634ffc0b07c53195e5d05c5604545d827c
lj <15535409713@msina.cn>
2025-05-03 07:59:49 +0800

    修改工程代码

---
+++ front-end-lj/src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    {{ title }}
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('这是主页容器')
+</script>
+<style lang="scss" scoped></style>
